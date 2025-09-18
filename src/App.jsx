88dd3c40269696c4d1a6b91f164f1ca098f12019
kap1793 <nap1793@gmail.com>
2025-09-18 11:39:51 -0400

    Initial render of my app with hardcoded data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import ComicCards from './ComicCards.jsx';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='header'>Comic Book Collections</h1>
+     <ComicCards />
+    </>
+  )
+}
+
+export default App
---
+++ src/ComicCards.jsx
@@ -0,0 +1,27 @@
+import comics from "./comics.js";
+
+function ComicCards() {
+    return (
+        <div className = "comicCards">
+            {comics.map((comic) => (
+                <div className="comicCard" key={comic.series + comic.issue}>
+                    <div className= "cardFlipFront">
+                        <img 
+                            className= "comicCoverArt"
+                            src={comic.coverArt}
+                            alt={comic.series + ": " + comic.issue}
+                        />
+                    </div>
+                    <div className = "cardFlipBack">
+                        <h2>{comic.series}: {comic.issue}</h2>
+                        <h3>{comic.releaseDate}</h3>
+                        <p>{comic.description}</p>
+                        <a href = {comic.webLink} className= "comicLink">Read it here!</a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ComicCards;
